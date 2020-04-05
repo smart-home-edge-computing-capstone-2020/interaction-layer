0a0ccf7f9e03747583ab26cd9daa894f5787629b
Niko Gupta <niko.gupta@gmail.com>
2020-04-05 12:39:41 -0400

    Add script to setup interaction layer installs

--- setup.sh
+++ setup.sh
@@ -1,32 +1,27 @@
-# Note: When this script is run, you should have cloned the repo found at TODO
+# Note: this script is intended for an Ubuntu vm.
+# Version: Ubuntu Server 18.04 LTS (HVM), 
+# AWS ami: ami-07ebfd5b3428b6f4d (64-bit x86) / ami-0400a1104d5b9caa1 (64-bit Arm)
+# I recommend an aws t2.micro
 
 # Set up top level modules
 sudo apt-get update
 sudo apt-get upgrade
-#sudo apt-get install gcc # needed for psutil
-#sudo apt-get install python3-devel # needed for psutil
+sudo apt-get install python3-venv
+sudo apt-get install python-pip
 
 # Install the broker
 sudo apt-get install mosquitto
 
 # Set up python virtual environment
-sudo apt-get install python3-venv
 python3 -m venv my_venv
 source my_venv/bin/activate
 
-# Install packages
-sudo apt-get install python-pip
+# Install requirements
 pip install --upgrade pip
-# TODO: pip install req
+pip install -r requirements.txt
 
 # Set up the database
 python3 setup_db.py
-
-# Set up the webapp environment
-sudo apt-get install npm
-#TODO: Need to go do npm install and stuff, clone repos
-# sudo npm install -g npm@latest
-# sudo npm install
 
 # Set up the CONFIG file TODO
 # Set up the SERIAL file TODO
