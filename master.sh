672b680890b4865c1e5ae541a55b59a4eb97ee0d
Niko Gupta <niko.gupta@gmail.com>
2020-04-05 12:35:49 -0400

    Change master from python script to bash script
    The actions that the master is doing are entirely bash commands. It is
    checking if processes are alive, and if not, restarting them. This is
    cleaner and more intuitive in bash rather than in python using the os
    library.

---
+++ master.sh
@@ -0,0 +1,33 @@
+# Time between checking processes
+SLEEP_TIME=1
+
+# Make the logs directory if it doesn't exist
+mkdir logs
+
+while [ true ]
+do
+    # Run the broker
+	if [[ !($(pgrep mosquitto)) ]]; then
+        echo 'mosquitto dead, restarting' >> logs/master.log
+		mosquitto -d -c mosquitto.conf
+	fi
+
+    # Run the webapp backend
+	if [[ !($(pgrep flask)) ]]; then
+        echo 'backend dead, restarting' >> logs/master.log
+		cd ../ecp-webapp/flask-backend
+		source my_venv/bin/activate
+		FLASK_APP=api.py FLASK_ENV=development flask run &>> backend.log &
+		cd ../../interaction-layer
+	fi
+
+    # Run the webapp frontend
+	if [[ !($(pgrep npm)) ]]; then
+        echo 'frontend dead, restarting' >> logs/master.log
+		cd ../ecp-webapp/ecp-frontend
+		npm start &>> frontend.log &
+		cd ../../interaction-layer
+	fi
+
+	sleep SLEEP_TIME
+done
