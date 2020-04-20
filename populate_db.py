012f9241c081156e00e927ecdb87b6bb9ea0b333
Niko Gupta <niko.gupta@gmail.com>
2020-04-20 10:37:19 -0400

    Change populate to nodes for demo

--- populate_db.py
+++ populate_db.py
@@ -7,12 +7,12 @@ node1 = {
         'ip_address' : '34.233.41.49',
         'is_master' : True,
         'is_broker' : True,
-        'is_device' : True,
+        'is_device' : False,
         'is_sensor' : True,
         'is_up' : True,
         'last_up' : int(time.time()),
-        'display_name' : 'Node 1',
-        'description' : 'Da first of da nodes'
+        'display_name' : 'Proximity Sensor 1',
+        'description' : 'Motion detector'
 }
 
 node2 = {
@@ -20,12 +20,12 @@ node2 = {
         'ip_address' : '18.208.23.252',
         'is_master' : False,
         'is_broker' : False,
-        'is_device' : True,
+        'is_device' : False,
         'is_sensor' : True,
         'is_up' : True,
         'last_up' : int(time.time()),
-        'display_name' : 'Node 2',
-        'description' : 'Da second of da nodes'
+        'display_name' : 'Proximity Sensor 2',
+        'description' : 'Motion detector'
 }
 
 node3 = {
@@ -34,13 +34,41 @@ node3 = {
         'is_master' : False,
         'is_broker' : False,
         'is_device' : True,
-        'is_sensor' : True,
+        'is_sensor' : False,
         'is_up' : True,
         'last_up' : int(time.time()),
-        'display_name' : 'Node 3',
-        'description' : 'Da third of da nodes'
+        'display_name' : 'Alarm Clock',
+        'description' : 'Wakes me up in the morning'
+}
+
+node4 = {
+        'serial' : 4,
+        'ip_address' : '18.210.201.94',
+        'is_master' : False,
+        'is_broker' : False,
+        'is_device' : True,
+        'is_sensor' : False,
+        'is_up' : True,
+        'last_up' : int(time.time()),
+        'display_name' : 'Coffee Pot',
+        'description' : 'This coffee was so expensive but it helps wake me up'
+}
+
+node5 = {
+        'serial' : 5,
+        'ip_address' : '50.16.106.15',
+        'is_master' : False,
+        'is_broker' : False,
+        'is_device' : True,
+        'is_sensor' : False,
+        'is_up' : True,
+        'last_up' : int(time.time()),
+        'display_name' : 'LED Lamp',
+        'description' : 'Makes my life more lit'
 }
 
 writeNodeToDb(node1)
 writeNodeToDb(node2)
 writeNodeToDb(node3)
+writeNodeToDb(node4)
+writeNodeToDb(node5)
